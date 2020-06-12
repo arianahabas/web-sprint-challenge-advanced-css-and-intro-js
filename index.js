9d0a893ca3f50eb66ac9477bd119e8ac53477501
arianahabas <arianahabas@gmail.com>
2020-06-12 11:17:18 -0500

    updated tasks

--- index.js
+++ index.js
@@ -208,10 +208,14 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0]);
 
+console.log(artists[2].bio)
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name="Vincent Van Gogh";
+console.log(artists[8]);
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -223,19 +227,20 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
-  
+  return (`The artist at index ${index} is ${array[index].name}.`)
+}
+    console.log(getArtistByIndex(artists, 8))
+
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who lived in the 20th century (1900-2000) */
 
-function get20s(/* Code here */){
-
-  /* Code here */
+function get20s(data){
 
 }
+
+
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,14 +253,15 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(arr,index) {
+    arr.splice(index,1)
+    console.log(arr.length)
   }
+
+  console.log(removeArtist(artists,0))
+  console.log(artists)
   
   /**
-
-
-
 
 /* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
 
@@ -269,7 +275,7 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should console.log() the new array with information added"*/
 
-function addArtist(/* Code here */){
+function addArtist(arr){
 
     /* Code here */
 
